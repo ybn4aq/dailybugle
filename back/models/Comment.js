cee32c59a2ab54a2e4f8aeb42e7609961cacc4d6
ahbey <ttw8ea@virginia.edu>
2024-11-09 15:51:24 -0500

    changed addComment function to add directly to article, changed functionality to mirror db structure (array of comments per article)

--- back/models/Comment.js
+++ back/models/Comment.js
@@ -1,13 +1,21 @@
 const connectDB = require("../db/connect");
 
-async function addComment(articleId, comment) {
+async function addCommentToArticle(articleId, commentData) {
   const db = await connectDB();
-  await db.collection("comments").insertOne({ articleId, ...comment });
+  const comment = {
+    ...commentData,
+    dateCreated: new Date()
+  };
+  await db.collection("articles").updateOne(
+    { _id: articleId },
+    { $push: { comments: comment } }
+  );
 }
 
 async function getComments(articleId) {
   const db = await connectDB();
-  return await db.collection("comments").find({ articleId }).toArray();
+  const article = await db.collection("articles").findOne({ _id: articleId });
+  return article ? article.comments : [];
 }
 
-module.exports = { addComment, getComments };
+module.exports = { addCommentToArticle, getComments };
