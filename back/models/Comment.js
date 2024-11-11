039f91fbd689514531c05f7db019fe0bd842d3d5
ahbey <ttw8ea@virginia.edu>
2024-11-10 21:50:28 -0500

    fixed functions in comment.js to use ObjectID

--- back/models/Comment.js
+++ back/models/Comment.js
@@ -1,4 +1,5 @@
 const connectDB = require("../db/connect");
+const { ObjectId } = require("mongodb"); // Import ObjectId
 
 async function addCommentToArticle(articleId, commentData) {
   const db = await connectDB();
@@ -8,16 +9,16 @@ async function addCommentToArticle(articleId, commentData) {
     user_id: commentData.user_id,
   };
   await db.collection("articles").updateOne(
-    { _id: articleId },
+    { _id: ObjectId(articleId) }, // Convert to ObjectId
     { $push: { comments: comment } }
   );
 }
 
-
 async function getComments(articleId) {
   const db = await connectDB();
-  const article = await db.collection("articles").findOne({ _id: articleId });
+  const article = await db.collection("articles").findOne({ _id: ObjectId(articleId) }); // Convert to ObjectId
   return article ? article.comments : [];
 }
+
 
 module.exports = { addCommentToArticle, getComments };
